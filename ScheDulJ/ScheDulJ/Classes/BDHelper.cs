14a8316e2e2ac61c36f578d4212fbbc2bac5ee7a
MMariatti <42209156+MMariatti@users.noreply.github.com>
2018-08-23 12:32:48 -0300

    Actualizacion Menor
    Cambio de distribucion de archivos, se agrego el BD Helper

---
+++ ScheDulJ/ScheDulJ/Classes/BDHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ScheDulJ
+{
+    class BDHelper
+    {
+        //CONSTRUCTOR DE LA CLASE 
+        public BDHelper()
+        {
+            cadenaConexion = "";
+            conexion = new SqlConnection();
+            comando = null; 
+        }
+        //CONSTRUCTOR ALTERNATIVO
+        public BDHelper(string cadenaConexion)
+        {
+            this.cadenaConexion = cadenaConexion;
+            conexion = new SqlConnection();
+            comando = new SqlCommand(); 
+        }
+        //ATRIBUTOS DE LA CLASE
+        private string cadenaConexion;
+        private SqlConnection conexion;
+        private SqlCommand comando; 
+        //PROPIEDAD QUE IMPLEMENTA SET Y GET DE CADA ATRIBUTO
+        public SqlConnection Conexion { get => conexion; set => conexion = value; }
+        public SqlCommand Comando { get => comando; set => comando = value; }
+        public string CadenaConexion { get => cadenaConexion; set => cadenaConexion = value; }
+        //METODO CONECTAR
+        public void Conectar()
+        {
+            conexion = new SqlConnection();
+            conexion.ConnectionString = this.CadenaConexion;
+            conexion.Open();
+            comando = new SqlCommand();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text; 
+        }
+       //METODO DESCONECTAR 
+        private void Desconectar()
+        {
+            conexion.Close();
+        }
+        //METODO CONSULTA GENERAL
+        public DataTable ConsultarTabla(string nombreTabla)
+        {
+            DataTable tabla = new DataTable();
+            Conectar();
+            comando.CommandText = "SELECT * FROM " + nombreTabla;
+            tabla.Load(comando.ExecuteReader());
+            Desconectar();
+            return tabla; 
+        }
+        //METODO CONSULTA GENERAL
+        public DataTable ConsultarSQL(string consultaSQL)
+        {
+            DataTable tabla = new DataTable();
+            Conectar();
+            comando.CommandText = consultaSQL;
+            tabla.Load(comando.ExecuteReader());
+            Desconectar();
+            return tabla;
+        }
+
+
+    }
+}
---
+++ ScheDulJ/ScheDulJ/Forms/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScheDulJ
+{
+    //El form MainMenu va a ser el principal de todo el programa. Este se iniciara cuando se loguee y 
+    //se mantendra abierto hasta que se termine de utilizar el programa. 
+    public partial class FrmMainMenu : Form
+    {
+        public FrmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void FrmMainMenu_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro de cerrar la aplicacion", "Cerrar Aplicacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                //El enviroment cierra toda la aplicacion sin tener que pasar por el evento de frmLogin_Closing 
+                //Sino se ejecuta dos veces la misma ventana 
+                Environment.Exit(1);
+            } 
+            else
+                e.Cancel = true;
+        }
+
+        private void btnExitMainMenu_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Esta seguro que desea cerrar la aplicacion?", "Cerrar Aplicacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Environment.Exit(1);
+            }
+        }
+    }
+}
